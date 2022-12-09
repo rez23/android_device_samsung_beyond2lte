0d8d75b2ff3ffb9585269b3ac618b7ec84c588e5
Spartaco Amadei <spamadei@gmail.com>
2022-12-09 04:09:48 +0100

    Passing to AOSP config

--- aosp_beyond2lte.mk
+++ aosp_beyond2lte.mk
@@ -34,16 +34,16 @@ TARGET_SCREEN_HEIGHT := 3040
 TARGET_SCREEN_WIDTH := 1440
 
 ## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_beyond2lte
+PRODUCT_NAME := aosp_beyond2lte
 PRODUCT_DEVICE := beyond2lte
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-G975F
+PRODUCT_MODEL := s10+
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
